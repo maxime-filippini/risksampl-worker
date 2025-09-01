1ea018d0b40f1450f5b24aa845d4f5abe76f5138
Maxime Filippini <maxime.filippini@gmail.com>
2025-09-01 10:02:52 +0200

    last update

--- scripts/brute_update_db.py
+++ scripts/brute_update_db.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env -S uv run
 
+# NOTE: Right now this script kind of expects tables to be cleaned so the relationships are honored.
+
 import pathlib
 from collections.abc import Sequence
 
 import polars as pl
 
+from worker.database import Base
+from worker.database import Instrument
+from worker.database import Investment
+from worker.database import Measure
+from worker.database import Portfolio
+from worker.database import PortfolioComposition
+from worker.database import PortfolioValue
 from worker.database import connect
+from worker.utils import insert_list_of_dicts
 
 DB_TABLES_PATH = pathlib.Path("/tmp/data/db")
+
+TABLE_MAP = {
+    "instruments": Instrument,
+    "portfolios": Portfolio,
+    "measures": Measure,
+    "ptf_comp": PortfolioComposition,
+    "ptf_values": PortfolioValue,
+    "investments": Investment,
+}
 
 
 def load_data(root_path: pathlib.Path):
@@ -20,25 +39,26 @@ def load_data(root_path: pathlib.Path):
     return out
 
 
-def _write_data(session, df: pl.DataFrame, table_name: str):
+def _write_data(session, df: pl.DataFrame, table_name: str, table: type[Base]):
     if len(df) < 50:
         print(f"Dataframe for {table_name} had less than 50 rows. Skipped.")
         return
 
-    df.write_database(table_name, session, if_table_exists="replace")
+    items = df.to_dicts()
+    insert_list_of_dicts(items, table=table, session=session)
 
 
 def main(argv: Sequence[str] | None = None) -> int:
     with connect() as session:
         dfs = load_data(DB_TABLES_PATH)
 
-        _write_data(session, dfs["instruments"], "instruments")
-        _write_data(session, dfs["portfolios"], "portfolios")
-        _write_data(session, dfs["investments"], "investments")
-        _write_data(session, dfs["market_data"], "market_data")
-        _write_data(session, dfs["ptf_comp"], "ptf_comp")
-        _write_data(session, dfs["ptf_values"], "ptf_values")
-        _write_data(session, dfs["measures"], "measures")
+        _write_data(session, dfs["instruments"], "instruments", table=TABLE_MAP["instruments"])
+        _write_data(session, dfs["portfolios"], "portfolios", table=TABLE_MAP["portfolios"])
+        _write_data(session, dfs["investments"], "investments", table=TABLE_MAP["investments"])
+        _write_data(session, dfs["market_data"], "market_data", table=TABLE_MAP["market_data"])
+        _write_data(session, dfs["ptf_comp"], "ptf_comp", table=TABLE_MAP["ptf_comp"])
+        _write_data(session, dfs["ptf_values"], "ptf_values", table=TABLE_MAP["ptf_values"])
+        _write_data(session, dfs["measures"], "measures", table=TABLE_MAP["measures"])
 
     return 0
 
